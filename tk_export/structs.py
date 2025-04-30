c5eec4dc609b413e12966fba04eb4133456fc200
Chris Hultin <chris.hultin@gmail.com>
2025-04-30 15:01:22 -0600

    Adding structs

--- tk_export/structs.py
+++ tk_export/structs.py
@@ -3,14 +3,14 @@ from dataclasses import dataclass
 import datetime
 
 @dataclass
-class settings:
+class Settings:
     user_id: str
-    cookie: str
+    cookie: typing.Dict[str, str]
     campaigns: typing.List[str]
 
 
 @dataclass
-class character_sheet:
+class CharacterSheet:
     character: str
 
     def to_json(self) -> dict:
@@ -19,11 +19,13 @@ class character_sheet:
         }
 
     @staticmethod
-    def from_json(data: dict) -> "character_sheet":
-        return character_sheet(character=data['character'])
+    def from_json(data: dict) -> "CharacterSheet":
+        if not data:
+            return
+        return CharacterSheet(character=data.get('character'))
 
 @dataclass
-class character_bio:
+class CharacterBio:
     background: str
     personality: str
     appearance: str
@@ -36,19 +38,21 @@ class character_bio:
         }
 
     @staticmethod
-    def from_json(data: dict) -> "character_bio":
-        return character_bio(background=data['background'],
+    def from_json(data: dict) -> "CharacterBio":
+        if not data:
+            return
+        return CharacterBio(background=data['background'],
                             personality=data['personality'],
                             appearance=data['appearance'])
 
 @dataclass
-class character:
+class Character:
     id: int
     name: str
     quote: str
     nickname: str
-    sheet: character_sheet
-    bio: character_bio
+    sheet: CharacterSheet
+    bio: CharacterBio
 
     def to_json(self) -> dict:
         return {
@@ -61,17 +65,17 @@ class character:
         }
 
     @staticmethod
-    def from_json(data: dict) -> "character":
-        return character(id=data['id'],
+    def from_json(data: dict) -> "Character":
+        return Character(id=data['id'],
                          name=data['name'],
                          quote=data['quote'],
                          nickname=data['nickname'],
-                         sheet=character_sheet.from_json(data['sheet']),
-                         bio=character_bio.from_json(data['bio'])
-        )
+                         sheet=CharacterSheet.from_json(data.get('sheet')),
+                         bio=CharacterBio.from_json(data.get('bio'))
+                         )
 
 @dataclass
-class campaign:
+class Campaign:
     created_at: datetime.datetime
     id: int
     name: str
@@ -81,15 +85,56 @@ class campaign:
         return {
             'id': self.id,
             'name': self.name,
-            'created_at': self.created_at,
+            'created_at': self.created_at.timestamp(),
             'system_name': self.system_name
         }
 
     @staticmethod
-    def from_json(data: dict) -> "campaign":
-        return campaign(created_at=datetime.datetime.fromisoformat(data['created_at']),
+    def from_json(data: dict, is_nano: bool = False) -> "Campaign":
+        created_timestamp = data['created_at'] / 1000 if is_nano else data['created_at']
+        return Campaign(created_at=created_timestamp,
                         id=data['id'],
                         name=data['name'],
                         system_name=data['system_name'])
 
+@dataclass
+class Roleplay:
+    created_at: datetime.datetime
+    id: int
+    name: str
+    def to_json(self) -> dict:
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at.timestamp(),
+        }
 
+    @staticmethod
+    def from_json(data: dict, is_nano: bool = False) -> "Roleplay":
+        created_timestamp = data['created_at'] / 1000 if is_nano else data['created_at']
+        return Roleplay(created_at=created_timestamp,
+                        id=data['id'],
+                        name=data['name'])
+
+@dataclass
+class RoleplayMessage:
+    character: Character
+    rerolls: int
+    roll: typing.List[dict]
+    content: str
+
+    def to_json(self) -> dict:
+        return {
+            'character': self.character.to_json(),
+            'rerolls': self.rerolls,
+            'roll': self.roll,
+            'content': self.content
+        }
+
+    @staticmethod
+    def from_json(data: dict) -> "RoleplayMessage":
+        character = Character.from_json(data['character'])
+        return RoleplayMessage(character=character,
+                               rerolls=data['rerolls'],
+                               roll=data['roll'],
+                               content=data['content'])
